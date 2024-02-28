1b934e35d7aa7d31007346eea02cf9d86fa0eb8c
Sardar5556 <abdulrehmanit582@gmail.com>
2024-02-28 16:59:48 +0500

    first commit

---
+++ cypress/Tripshepherd/Test1.js
@@ -0,0 +1,24 @@
+Cypress.on('uncaught:exception', (err, runnable) => {
+    return false;
+});
+
+describe('Visit and Click on First Product', () => {
+    it('should visit the website, find the first product, and click', () => {
+        cy.viewport(1920, 1080);
+        cy.visit('https://www.tripshepherd.com/');
+
+        cy.contains('See More').click();
+        cy.contains('Best of San Antonio Tour').click();
+      
+        cy.wait(2000);
+        cy.get('.gap-3 > .border.w-full').click();
+        cy.get('.react-calendar__month-view__days > :nth-child(28)').click();
+        cy.contains('Book now').click();
+    
+        cy.wait(2000);
+        cy.get(':nth-child(1) > .p-2').type('Sardar');
+        cy.get(':nth-child(2) > .p-2').type('sardar123@gmail.com');
+        cy.get('.PhoneInputCountrySelect').select('Pakistan');
+        cy.get('.PhoneInputInput').type('3335566333');
+    });
+});
