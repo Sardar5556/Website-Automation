19e1f10740bcd3f9323e53b9d084ac719f3f6ae5
Sardar Abdul Rehman <156429633+Sardar5556@users.noreply.github.com>
2024-03-04 18:12:15 +0500

    Update Test1.js

--- cypress/Tripshepherd/Test1.js
+++ cypress/Tripshepherd/Test1.js
@@ -12,7 +12,7 @@ describe('Visit and Click on the Product', () => {
       
         cy.wait(2000);
         cy.get('.gap-3 > .border.w-full').click();
-        cy.get('.react-calendar__month-view__days > :nth-child(28)').click();
+        cy.get('.react-calendar__month-view__days > :nth-child(8)').click();
         cy.contains('Book now').click();
     
         cy.wait(2000);
