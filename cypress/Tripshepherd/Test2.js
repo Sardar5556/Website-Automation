3ee46d94437f287219373cfcbfc20ba5aab7eddb
Sardar5556 <156429633+Sardar5556@users.noreply.github.com>
2024-03-04 17:19:18 +0500

    Added Cypress fixtures

---
+++ cypress/Tripshepherd/Test2.js
@@ -0,0 +1,26 @@
+Cypress.on('uncaught:exception', (err, runnable) => {
+    return false;
+});
+
+describe('Visit and Click on First Product', () => {
+    it('should visit the website, find the first product, and click', () => {
+        cy.viewport(1920, 1080);
+        cy.visit('https://www.tripshepherd.com/');
+
+        cy.contains('See More').click();
+        cy.contains('Best of San Antonio Tour').click();
+      
+        cy.wait(4000);
+        cy.get('.gap-3 > .border.w-full').click();
+        cy.get('.react-calendar__month-view__days > :nth-child(6)').click();
+        cy.contains('Book now').click();
+    
+        cy.wait(4000);
+        cy.fixture('testdata.json').then((data) => {
+            cy.get(':nth-child(1) > .p-2').type(data.name);
+            cy.get(':nth-child(2) > .p-2').type(data.email);
+            cy.get('.PhoneInputCountrySelect').select(data.country);
+            cy.get('.PhoneInputInput').type(data.phone);
+        });
+    });
+});
